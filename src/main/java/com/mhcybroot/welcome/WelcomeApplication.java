9cec7b0878d1104c7d0cf0e9fdd91912a3cc02fe
mhcybrooted <honey.app.developer@gmail.com>
2025-10-17 16:10:26 +0600

    completed

---
+++ src/main/java/com/mhcybroot/welcome/WelcomeApplication.java
@@ -0,0 +1,13 @@
+package com.mhcybroot.welcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //2
+public class WelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeApplication.class, args);
+	}
+
+}
